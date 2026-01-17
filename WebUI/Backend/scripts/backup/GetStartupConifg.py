541d473dba77c0d7ee709b227da6f4d8bba0bc38
AmaarElgerzawy <snipergolden1234@gmail.com>
2026-01-17 19:02:05 +0200

    fix paths

--- WebUI/Backend/scripts/backup/GetStartupConifg.py
+++ WebUI/Backend/scripts/backup/GetStartupConifg.py
@@ -2,7 +2,7 @@ from netmiko import ConnectHandler
 from getpass import getpass
 from pathlib import Path
 from datetime import datetime
-from constants import SSH_USERNAME, SSH_PASSWORD, SSH_ENABLE_PASSWORD, BASEDIR
+from ...constants import SSH_USERNAME, SSH_PASSWORD, SSH_ENABLE_PASSWORD, BASEDIR
 
 # ---------- SETTINGS ----------
 ROUTER_LIST_FILE = BASEDIR / "scripts/backup" / "routers.txt"
